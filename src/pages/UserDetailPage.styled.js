87810e9b904cbb17d01fd0f8426f0617dcb395e7
nadine525 <02_seeders_wide@icloud.com>
2023-08-15 16:01:20 +0200

    style

--- src/pages/UserDetailPage.styled.js
+++ src/pages/UserDetailPage.styled.js
@@ -2,16 +2,14 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const StyledLink = styled(NavLink)`
-  margin-right: 50px;
   border: 1px solid black;
   border-radius: 3px;
   color: black;
   cursor: pointer;
   display: inline-block;
   font-family: 'Roboto', sans-serif;
-  font-size: 12px;
-  height: 20px;
   font-size: 14px;
+  height: 20px;
   padding: 0 10px;
   text-align: center;
   background-color: lightgray;
